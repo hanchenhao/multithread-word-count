d3c8b9cac6196e4218f40f233850f1027bb15418
hanchenhao <hanchenhao@vip.qq.com>
2023-02-07 14:06:56 +0800

    WordCount Practice

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,17 @@
 package com.github.hcsp.multithread;
 
-import java.io.File;
+import java.io.*;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.*;
 
 public class MultiThreadWordCount1 {
+
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws IOException, ExecutionException, InterruptedException {
+        WordCount wordCount = new WordCount(threadNum);
+        return wordCount.filesCount(files);
     }
 }
+
+
